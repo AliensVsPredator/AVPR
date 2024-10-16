8b021331d55fa6bf30d28011044f4b584f2f5f08
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-10-16 19:24:57 -0400

    Add code for hosts tag, tag to be done later

--- common/src/main/java/org/avpr/common/tags/AVPREntityTags.java
+++ common/src/main/java/org/avpr/common/tags/AVPREntityTags.java
@@ -10,4 +10,5 @@ public class AVPREntityTags {
 
     public static final TagKey<EntityType<?>> ACID_IMMUNE = CommonMod.create(Registries.ENTITY_TYPE,"acid_immune");
     public static final TagKey<EntityType<?>> ALIENS = CommonMod.create(Registries.ENTITY_TYPE,"aliens");
+    public static final TagKey<EntityType<?>> HOSTS = CommonMod.create(Registries.ENTITY_TYPE,"hosts");
 }
