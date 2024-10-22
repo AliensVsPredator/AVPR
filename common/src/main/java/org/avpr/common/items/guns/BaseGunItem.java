d6d6f985cfa01b97b897157bd74a270d0bf17a3e
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-10-22 15:38:23 -0400

    Fixes sounds/shooting logic for other guns

--- common/src/main/java/org/avpr/common/items/guns/BaseGunItem.java
+++ common/src/main/java/org/avpr/common/items/guns/BaseGunItem.java
@@ -1,6 +1,7 @@
 package org.avpr.common.items.guns;
 
 import mod.azure.azurelib.common.api.common.animatable.GeoItem;
+import mod.azure.azurelib.common.api.common.helper.CommonUtils;
 import mod.azure.azurelib.common.internal.client.RenderProvider;
 import mod.azure.azurelib.common.internal.common.animatable.SingletonGeoAnimatable;
 import mod.azure.azurelib.common.internal.common.util.AzureLibUtil;
@@ -12,7 +13,6 @@ import mod.azure.azurelib.core.object.PlayState;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
@@ -23,20 +23,19 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
-import org.avpr.common.api.common.ItemUtil;
-import org.avpr.common.registries.AVPRItems;
-import org.avpr.common.tags.AVPRItemTags;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.function.Consumer;
 
-import org.avpr.common.api.util.PredicatesUtil;
+import org.avpr.common.api.common.ItemUtil;
 import org.avpr.common.api.util.TooltipUtils;
 import org.avpr.common.client.items.renders.GunRender;
 import org.avpr.common.entities.projectiles.BulletProjectile;
 import org.avpr.common.registries.AVPRDataComponments;
+import org.avpr.common.registries.AVPRItems;
 import org.avpr.common.registries.AVPRSounds;
+import org.avpr.common.tags.AVPRItemTags;
 
 public class BaseGunItem extends Item implements GeoItem {
 
@@ -73,7 +72,9 @@ public class BaseGunItem extends Item implements GeoItem {
             tooltipComponents,
             "tooltip.avpr.ammunition",
             Component.literal(
-                stack.get(AVPRDataComponments.CURRENT_AMMO.get()) + " / " + stack.get(AVPRDataComponments.MAX_AMMO.get())
+                stack.get(AVPRDataComponments.CURRENT_AMMO.get()) != null
+                    ? stack.get(AVPRDataComponments.CURRENT_AMMO.get()).toString()
+                    : "0" + " / " + stack.get(AVPRDataComponments.MAX_AMMO.get())
             )
         );
         // TooltipUtils.appendLabel(
@@ -113,10 +114,28 @@ public class BaseGunItem extends Item implements GeoItem {
     public void inventoryTick(@NotNull ItemStack stack, @NotNull Level level, @NotNull Entity entity, int slotId, boolean isSelected) {
         super.inventoryTick(stack, level, entity, slotId, isSelected);
         if (stack.get(AVPRDataComponments.CURRENT_AMMO.get()) == null)
-            stack.set(AVPRDataComponments.CURRENT_AMMO.get(), entity instanceof Player player && player.getAbilities().instabuild ? 0 : stack.get(AVPRDataComponments.MAX_AMMO.get()));
-        if (!level.isClientSide() && stack.get(AVPRDataComponments.STOP_ANIMATIONS.get()) != null && stack.get(AVPRDataComponments.STOP_ANIMATIONS.get()) && entity instanceof Player player)
+            stack.set(
+                AVPRDataComponments.CURRENT_AMMO.get(),
+                entity instanceof Player player && player.getAbilities().instabuild ? 0 : stack.get(AVPRDataComponments.MAX_AMMO.get())
+            );
+        if (
+            !level.isClientSide() && stack.get(AVPRDataComponments.STOP_ANIMATIONS.get()) != null && stack.get(
+                AVPRDataComponments.STOP_ANIMATIONS.get()
+            ) && entity instanceof Player player
+        ) {
             triggerAnim(player, GeoItem.getOrAssignId(stack, (ServerLevel) level), "main", "idle");
-
+            if (this.getItemID().matches("weapon_old_painless"))
+                level.playSound(
+                    null,
+                    player.blockPosition(),
+                    AVPRSounds.ITEM_WEAPON_OLD_PAINLESS_SHOOT_STOP.get(),
+                    SoundSource.PLAYERS,
+                    1,
+                    1
+                );
+            stack.set(AVPRDataComponments.STOP_ANIMATIONS.get(), false);
+        }
+        stack.getOrDefault(AVPRDataComponments.GUN_HAS_WINDUP.get(), false);
     }
 
     @Override
@@ -124,52 +143,59 @@ public class BaseGunItem extends Item implements GeoItem {
         super.onUseTick(level, livingEntity, stack, remainingUseDuration);
         if (livingEntity instanceof Player player) {
             if (stack.get(AVPRDataComponments.CURRENT_AMMO.get()) > 0 || player.getAbilities().instabuild) {
-                if (!stack.has(AVPRDataComponments.GUN_HAS_WINDUP.get()) && !player.getCooldowns().isOnCooldown(this)) {
-                    this.fireBullet(player, stack, level);
-                    player.getCooldowns().addCooldown(this, stack.get(AVPRDataComponments.GUN_FIRERATE.get()));
-                    level.playSound(
-                            null,
-                            player.blockPosition(),
-                            AVPRSounds.ITEM_WEAPON_SHOTGUN_SHOOT.get(),
-                            SoundSource.PLAYERS,
-                            1,
-                            1
-                    );
-                    if (!player.getAbilities().instabuild)
-                        stack.set(AVPRDataComponments.CURRENT_AMMO.get(),
-                                stack.get(AVPRDataComponments.CURRENT_AMMO.get()) - 1);
-                } else {
+                 if (this.getItemID().matches("weapon_old_painless")) {
                     this.windUpTicks++;
                     stack.set(AVPRDataComponments.STOP_ANIMATIONS.get(), false);
                     if (stack.get(AVPRDataComponments.GUN_HAS_WINDUP.get())) {
                         level.playSound(
-                                null,
-                                player.blockPosition(),
-                                AVPRSounds.ITEM_WEAPON_OLD_PAINLESS_SHOOT_SPINNING.get(),
-                                SoundSource.PLAYERS,
-                                1,
-                                1
+                            null,
+                            player.blockPosition(),
+                            AVPRSounds.ITEM_WEAPON_OLD_PAINLESS_SHOOT_STOP.get(),
+                            SoundSource.PLAYERS,
+                            1,
+                            1
                         );
                         if (!level.isClientSide())
                             triggerAnim(player, GeoItem.getOrAssignId(stack, (ServerLevel) level), "main", "spin");
                         stack.set(AVPRDataComponments.GUN_HAS_WINDUP.get(), false);
                     }
-                    if (this.windUpTicks > stack.get(AVPRDataComponments.GUN_WIND_UP_LONG.get()) * 2) {
+                    if (this.windUpTicks > stack.get(
+                            AVPRDataComponments.GUN_WIND_UP_LONG.get()
+                        )
+                    ) {
                         this.fireBullet(player, stack, level);
                         level.playSound(
-                                null,
-                                player.blockPosition(),
-                                AVPRSounds.ITEM_WEAPON_SHOTGUN_SHOOT.get(),
-                                SoundSource.PLAYERS,
-                                1,
-                                1
+                            null,
+                            player.blockPosition(),
+                            AVPRSounds.ITEM_WEAPON_OLD_PAINLESS_SHOOT_LOOP.get(),
+                            SoundSource.PLAYERS,
+                            1,
+                            1
                         );
-                        if (!level.isClientSide())
+                        if (!level.isClientSide() && this.getItemID().matches("weapon_old_painless"))
                             triggerAnim(player, GeoItem.getOrAssignId(stack, (ServerLevel) level), "main", "spin");
                         if (!player.getAbilities().instabuild)
                             stack.set(AVPRDataComponments.CURRENT_AMMO.get(), stack.get(AVPRDataComponments.CURRENT_AMMO.get()) - 1);
                     }
-                }
+                } else {
+                     if (!player.getCooldowns().isOnCooldown(this)) {
+                         this.fireBullet(player, stack, level);
+                         player.getCooldowns().addCooldown(this, stack.get(AVPRDataComponments.GUN_FIRERATE.get()));
+                         level.playSound(
+                                 null,
+                                 player.blockPosition(),
+                                 AVPRSounds.ITEM_WEAPON_SHOTGUN_SHOOT.get(),
+                                 SoundSource.PLAYERS,
+                                 1,
+                                 1
+                         );
+                         if (!player.getAbilities().instabuild)
+                             stack.set(
+                                     AVPRDataComponments.CURRENT_AMMO.get(),
+                                     stack.get(AVPRDataComponments.CURRENT_AMMO.get()) - 1
+                             );
+                     }
+                 }
             } else if (player.getInventory().hasAnyMatching(itemStack -> itemStack.is(AVPRItemTags.HEAVY_AMMO))) {
                 if (player.getInventory().countItem(AVPRItems.BULLET_HEAVY.get()) > 0)
                     ItemUtil.reloadGun(stack, player, this);
@@ -200,7 +226,9 @@ public class BaseGunItem extends Item implements GeoItem {
             livingEntity instanceof Player player && (stack.get(AVPRDataComponments.CURRENT_AMMO.get()) > 0
                 || player.getAbilities().instabuild)
         ) {
-            var bulletEntity = new BulletProjectile(level, stack.get(AVPRDataComponments.GUN_DAMAGE.get()));
+            var bulletEntity = new BulletProjectile(level, stack.get(AVPRDataComponments.GUN_DAMAGE.get()), false);
+            if (this.getItemID().matches("weapon_m56_smartgun"))
+                bulletEntity = new BulletProjectile(level, stack.get(AVPRDataComponments.GUN_DAMAGE.get()), true);
             bulletEntity.shootFromRotation(
                 livingEntity,
                 livingEntity.getXRot(),
@@ -209,6 +237,7 @@ public class BaseGunItem extends Item implements GeoItem {
                 3.0F,
                 1.0F
             );
+            CommonUtils.spawnLightSource(livingEntity, player.level().isWaterAt(player.blockPosition()));
             bulletEntity.moveTo(livingEntity.getX(), livingEntity.getY(0.6D), livingEntity.getZ(), 0, 0);
             level.addFreshEntity(bulletEntity);
         }
@@ -240,10 +269,11 @@ public class BaseGunItem extends Item implements GeoItem {
             new AnimationController<>(this, "main", event -> PlayState.CONTINUE).triggerableAnim(
                 "spin",
                 RawAnimation.begin().thenLoop("barrelspin")
-            ).triggerableAnim(
+            )
+                .triggerableAnim(
                     "idle",
                     RawAnimation.begin().thenPlay("idle")
-            )
+                )
         );
     }
 
