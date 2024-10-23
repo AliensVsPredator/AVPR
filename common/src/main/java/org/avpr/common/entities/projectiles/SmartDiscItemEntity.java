5132069b678e8b74ecdafa4d14dd23b51ef7e9ba
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-10-23 18:48:52 -0400

    Smart Discs now return to the user.

--- common/src/main/java/org/avpr/common/entities/projectiles/SmartDiscItemEntity.java
+++ common/src/main/java/org/avpr/common/entities/projectiles/SmartDiscItemEntity.java
@@ -2,6 +2,7 @@ package org.avpr.common.entities.projectiles;
 
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
@@ -16,6 +17,8 @@ import org.avpr.common.registries.AVPREntities;
 import org.avpr.common.registries.AVPRItems;
 
 public class SmartDiscItemEntity extends ThrowableItemProjectile {
+
+    private boolean dealtDamage;
 
     public SmartDiscItemEntity(EntityType<? extends ThrowableItemProjectile> entityType, Level level) {
         super(entityType, level);
@@ -33,10 +36,24 @@ public class SmartDiscItemEntity extends ThrowableItemProjectile {
     @Override
     public void tick() {
         super.tick();
-        // If older then 15 seconds, remove to keep entity count down
         if (this.tickCount > 300)
             this.kill();
-        EntityUtil.trackToLivingEntity(this, 0.5, false);
+        if (!this.dealtDamage)
+            EntityUtil.trackToLivingEntity(this, 0.5, false);
+        if (this.dealtDamage && this.getOwner() != null) {
+            var vec3 = this.getOwner().getEyePosition().subtract(this.position());
+            this.setPosRaw(this.getX(), this.getY() + vec3.y * 0.015, this.getZ());
+            if (this.level().isClientSide)
+                this.yOld = this.getY();
+            this.setDeltaMovement(this.getDeltaMovement().scale(0.95).add(vec3.normalize().scale(0.5)));
+            if (this.getOwner() instanceof Player player && this.getBoundingBox().intersects(this.getOwner().getBoundingBox())) {
+                player.getInventory().add(AVPRItems.SMART_DISC.get().getDefaultInstance());
+                this.kill();
+            } else if (this.getBoundingBox().intersects(this.getOwner().getBoundingBox()))
+                this.kill();
+        }
+        if (this.getOwner() == null)
+            this.kill();
     }
 
     @Override
@@ -46,18 +63,18 @@ public class SmartDiscItemEntity extends ThrowableItemProjectile {
             result.getBlockPos(),
             CommonMod.config.itemConfigs.SMARTDISC_BREAKSPEED_MODIFIER * 2.0F
         );
-        this.kill();
+        this.dealtDamage = true;
         super.onHitBlock(result);
     }
 
     @Override
     protected void onHitEntity(@NotNull EntityHitResult result) {
-        if (result.getEntity() instanceof LivingEntity livingEntity && getOwner() != null) {
+        if (result.getEntity() instanceof LivingEntity livingEntity && getOwner() != null && livingEntity != getOwner()) {
             livingEntity.hurt(
                 damageSources().thrown(getOwner(), livingEntity),
                 CommonMod.config.itemConfigs.SMARTDISC_DAMAGE
             );
-            this.kill();
+            this.dealtDamage = true;
         }
         super.onHitEntity(result);
     }
