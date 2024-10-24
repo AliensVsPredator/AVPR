f10dd84fa06070d360f3a31243384a25577898ec
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-10-24 08:56:18 -0400

    Added TODO for armor items

--- common/src/main/java/org/avpr/common/items/armor/AVPRArmorItem.java
+++ common/src/main/java/org/avpr/common/items/armor/AVPRArmorItem.java
@@ -4,6 +4,14 @@ import net.minecraft.core.Holder;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 
+/**
+ * TODO:
+ * <ul>
+ * <li>Move to {@link mod.azure.azurelib.common.api.common.animatable.GeoItem} to add the new armor.</li>
+ * <li>Get the flashlight working on tactical armor chestplate using the new
+ * {@link org.avpr.common.entities.LightEntity}.</li>
+ * </ul>
+ */
 public class AVPRArmorItem extends ArmorItem {
 
     public AVPRArmorItem(Holder<ArmorMaterial> material, Type type, Properties properties) {
