242b8c483730b2dd30d58b5c294e60f70a33ed88
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-10-24 08:55:08 -0400

    use a better variable name

--- common/src/main/java/org/avpr/common/items/ShurikenItem.java
+++ common/src/main/java/org/avpr/common/items/ShurikenItem.java
@@ -80,13 +80,11 @@ public class ShurikenItem extends Item {
      * @return The calculated power level for the given charge time.
      */
     public static float getPowerForTime(int charge) {
-        float f = charge / 20.0F;
-        f = (f * f + f * 2.0F) / 3.0F;
-        if (f > 1.0F) {
-            f = 1.0F;
-        }
-
-        return f;
+        var normalizedCharge = charge / 20.0F;
+        normalizedCharge = (normalizedCharge * normalizedCharge + normalizedCharge * 2.0F) / 3.0F;
+        if (normalizedCharge > 1.0F)
+            normalizedCharge = 1.0F;
+        return normalizedCharge;
     }
 
     /**
